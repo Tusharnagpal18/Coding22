e3305d98979ca15badf9271b63d541f1d5a8ff52
Tusharnagpal18 <68907030+Tusharnagpal18@users.noreply.github.com>
2023-02-28 23:11:53 +0530

    Create LC#645.java

---
+++ LC#645.java
@@ -0,0 +1,24 @@
+You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+
+You are given an integer array nums representing the data status of this set after the error.
+
+Find the number that occurs twice and the number that is missing and return them in the form of an array.
+===================sol===========
+class Solution {
+        public int[] findErrorNums(int[] nums) {
+        
+            int dup=-1,miss=-1,count=0;
+            for(int i=1;i<=nums.length;i++){
+                count=0;
+                for(int j=0;j<nums.length;j++){
+                    if(nums[j]==i)
+                    count++;
+                }
+                if(count==2)
+                  dup=i;
+                else if(count==0)
+                  miss=i;  
+            }
+        return new int[]{dup,miss};
+    }
+}

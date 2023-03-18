9848b194cff37c58ecc5060e8728dc70a3777128
Tusharnagpal18 <68907030+Tusharnagpal18@users.noreply.github.com>
2023-03-18 23:44:26 +0530

    Update LC#1464.java

--- LC#1464.java
+++ LC#1464.java
@@ -2,6 +2,7 @@
 
 Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
 ===========SOL=======
+    
 
 class Solution{
 

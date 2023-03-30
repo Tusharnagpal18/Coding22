5e89feb98b688406990e356b618592e2a9ecec24
Tusharnagpal18 <68907030+Tusharnagpal18@users.noreply.github.com>
2023-03-30 08:56:16 +0530

    Update LC#342.java

--- LC#342.java
+++ LC#342.java
@@ -1,4 +1,4 @@
-Given an integer n, return true if it is a power of four. Otherwise, return false.
+                Given an integer n, return true if it is a power of four. Otherwise, return false.
 
 An integer n is a power of four, if there exists an integer x such that n == 4x.
   

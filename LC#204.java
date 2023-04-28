49d29fa32466bf17819e1a48426eaea02f034369
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2023-04-28 23:23:34 +0530

    Update LC#204.java

--- LC#204.java
+++ LC#204.java
@@ -9,7 +9,7 @@ Given an integer n, return the number of prime numbers that are strictly less th
        int ctr=0;
        for(int i=2;i<=Math.sqrt(n);i++){
            if(ans[i]==false){
-            //    marking all multiples of i as true
+              //    marking all multiples of i as true 
                for(int j=i*i;j<n;j+=i){
                    ans[j]=true;
                }

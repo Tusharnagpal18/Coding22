5886b04184a7cca3d787c6533ad1dfc693570311
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2023-05-02 19:49:06 +0530

    Create LC#1822.java

---
+++ LC#1822.java
@@ -0,0 +1,25 @@
+1822. Sign of the Product of an Array
+There is a function signFunc(x) that returns:
+
+1 if x is positive.
+-1 if x is negative.
+0 if x is equal to 0.
+You are given an integer array nums. Let product be the product of all values in the array nums.
+
+Return signFunc(product)
+  
+  ====sol======
+  class Solution {
+    public int arraySign(int[] nums) {
+        int ctr=0;
+        for(int i:nums){
+            if(i==0)
+              return 0;  
+            if(i<0)
+               ctr++;
+        }
+        if(ctr%2==0)
+          return 1;
+        return -1;  
+    }
+}

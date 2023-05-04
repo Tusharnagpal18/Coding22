66262d6f28241179fc797722a2d60579a8b0f868
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2023-05-04 22:41:43 +0530

    Update LC#1528.java

--- LC#1528.java
+++ LC#1528.java
@@ -1,6 +1,5 @@
 //1528. Shuffle String
 you are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
-
 ----sol-------
 class Solution {
     public String restoreString(String s, int[] indices) {

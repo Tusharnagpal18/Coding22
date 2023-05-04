6c6d15be2100596f4ea15efaf39cd6cbe197ff59
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2023-05-04 22:39:29 +0530

    Update LC#1528.java

--- LC#1528.java
+++ LC#1528.java
@@ -6,7 +6,6 @@ class Solution {
     public String restoreString(String s, int[] indices) {
         char[] str=new char[s.length()];
         for(int i=0;i<s.length();i++){
-
             str[indices[i]]=s.charAt(i);
         }
         return new String(str);

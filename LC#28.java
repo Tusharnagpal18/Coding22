5068097ec435b91c75164de1cece5249a5a7c029
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2023-05-08 21:04:55 +0530

    Update LC#28.java

--- LC#28.java
+++ LC#28.java
@@ -1,6 +1,7 @@
 28. Find the Index of the First Occurrence in a String
 Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
-
+ 
+ 
  ===============SOL===========
  class Solution {
     public int strStr(String haystack, String needle) {

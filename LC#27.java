00342721cff6e9c6932bc40f96bc4a77abeaed3d
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2023-05-14 09:45:33 +0530

    Update LC#27.java

--- LC#27.java
+++ LC#27.java
@@ -4,8 +4,9 @@ Given an integer array nums and an integer val, remove all occurrences of val in
 Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
 
 Return k after placing the final result in the first k slots of nums.
-  
-  =========solution=======
+
+        
+ ==========solution=======
   class Solution {
     public int removeElement(int[] nums, int val) {
         int i = 0;

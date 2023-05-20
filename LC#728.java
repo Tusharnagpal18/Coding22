b88db0d022994acd5e846733431ae00119944d28
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2023-05-20 23:30:58 +0530

    Update LC#728.java

--- LC#728.java
+++ LC#728.java
@@ -5,7 +5,6 @@ For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, a
 A self-dividing number is not allowed to contain the digit zero.
 
 Given two integers left and right, return a list of all the self-dividing numbers in the range [left, right]
-
 ========sol=========
 class Solution {
     public List<Integer> selfDividingNumbers(int left, int right) {

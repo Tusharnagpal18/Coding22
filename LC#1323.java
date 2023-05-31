2a89d7cc201dabcd38ddb7d69c3e639dd993eac2
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2023-05-31 19:31:42 +0530

    Update LC#1323.java

--- LC#1323.java
+++ LC#1323.java
@@ -1,4 +1,5 @@
 1323. Maximum 69 Number
+
 You are given a positive integer num consisting only of digits 6 and 9.
 
 Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).

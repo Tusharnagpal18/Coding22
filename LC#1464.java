eb13709255ff98c3aab759991a72412d0766b2df
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2023-06-01 23:53:04 +0530

    Update LC#1464.java

--- LC#1464.java
+++ LC#1464.java
@@ -2,8 +2,6 @@
 
 Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
 ===========SOL=======
-    
-
 class Solution{
 
 public int maxProduct(int[] nums) {

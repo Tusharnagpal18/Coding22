195a8123321da2dbf11f5e761b3ac6b6e493b705
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2023-06-01 23:53:13 +0530

    Update LC#1539.java

--- LC#1539.java
+++ LC#1539.java
@@ -3,7 +3,6 @@
 Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
 
 Return the kth positive integer that is missing from this array.
-
  ============SOL==========
  class Solution {
     public int findKthPositive(int[] arr, int k) {

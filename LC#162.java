cef7f5f176db61a1e943c804b1a7d6cdd132864c
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2023-06-03 23:50:24 +0530

    Update LC#162.java

--- LC#162.java
+++ LC#162.java
@@ -1,7 +1,6 @@
                                                162.     
                                         Find Peak Element
 
-
 A peak element is an element that is strictly greater than its neighbors.
 
 Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.

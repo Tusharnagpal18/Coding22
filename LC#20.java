174141217a5a5499ee2f0caaea8525a3099b59b4
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2023-06-13 23:58:40 +0530

    Update LC#20.java

--- LC#20.java
+++ LC#20.java
@@ -1,3 +1,11 @@
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> st=new Stack<Character>();

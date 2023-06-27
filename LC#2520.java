0646901f8fbbb5b44e95114bc4e05f4341a45f87
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2023-06-27 19:40:23 +0530

    Update LC#2520.java

--- LC#2520.java
+++ LC#2520.java
@@ -3,10 +3,8 @@
 Given an integer num, return the number of digits in num that divide num.
 
 An integer val divides nums if nums % val == 0.
-
                       
 --------------Solution-------
-
 class Solution {
     public int countDigits(int num) {
         int num2=num,r=0,ctr=0;

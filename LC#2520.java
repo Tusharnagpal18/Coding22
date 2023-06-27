71c9bce64229a452ffce727c82349d7e99ca1584
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2023-06-27 19:33:51 +0530

    Update LC#2520.java

--- LC#2520.java
+++ LC#2520.java
@@ -3,7 +3,8 @@
 Given an integer num, return the number of digits in num that divide num.
 
 An integer val divides nums if nums % val == 0.
-  
+
+                      
 --------------Solution-------
 
 class Solution {

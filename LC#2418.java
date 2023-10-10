4ed97aee13bd66b057342388e6a2584ad80d2a71
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2023-10-10 17:26:53 +0530

    Update LC#2418.java

--- LC#2418.java
+++ LC#2418.java
@@ -2,7 +2,6 @@
 You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n.
 
 For each index i, names[i] and heights[i] denote the name and height of the ith person.
-  
 Return names sorted in descending order by the people's heights
   class Solution {
     public String[] sortPeople(String[] names, int[] ht) {

447ded21cb03afa300dcdb344bb9118c69337bc4
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2023-10-16 22:07:13 +0530

    Update LC#152.java

--- LC#152.java
+++ LC#152.java
@@ -2,9 +2,7 @@ Maximum Product Subarray
 Given an integer array nums, find a 
 subarray
  that has the largest product, and return the product.
-The test cases are generated so that the answer will fit in a 32-bit integer.
-
- 
+The test cases are generated so that the answer will fit in a 32-bit integer. 
 
   ===============sol=============
   class Solution {

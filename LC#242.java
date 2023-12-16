7909e7ba5e38b2b2441075c236bc8179fd7b5695
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2023-12-16 22:51:15 +0530

    Create LC#242.java

---
+++ LC#242.java
@@ -0,0 +1,21 @@
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+  ======sol===========
+  class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())
+          return false;
+        int ans[]=new int[26];
+        for(char i:s.toCharArray())
+           ans[i-'a']++;
+        for(char i:t.toCharArray())
+           ans[i-'a']--;
+        for(int i:ans){
+           if(i!=0){
+            return false;      
+           }
+        }
+    return true;
+    }
+}

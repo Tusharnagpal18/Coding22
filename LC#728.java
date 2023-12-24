cbce240a6471b32ecab3b2945813dcf9ca9c596b
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2023-12-24 22:41:46 +0530

    Update LC#728.java

--- LC#728.java
+++ LC#728.java
@@ -1,6 +1,5 @@
 728. Self Dividing Numbers
 A self-dividing number is a number that is divisible by every digit it contains.
-    
 For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
 A self-dividing number is not allowed to contain the digit zero.
 

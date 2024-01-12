ee607d9d9ac29dd0313313cbee58d829ae69a70c
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2024-01-12 22:19:39 +0530

    Update LC#2264.java

--- LC#2264.java
+++ LC#2264.java
@@ -1,6 +1,6 @@
 You are given a string num representing a large integer. An integer is good if it meets the following conditions:
 
-It is a substring of num with length 3.
+It is a substring of num with length3.
 It consists of only one unique digit.
 Return the maximum good integer as a string or an empty string "" if no such integer exists.
 

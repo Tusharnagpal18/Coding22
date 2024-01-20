9b2740dfac988ef1effbe097840a33f120dd6720
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2024-01-20 23:27:51 +0530

    Create LC#1217.java

---
+++ LC#1217.java
@@ -0,0 +1,23 @@
+We have n chips, where the position of the ith chip is position[i].
+
+We need to move all the chips to the same position. In one step, we can change the position of the ith chip from position[i] to:
+
+position[i] + 2 or position[i] - 2 with cost = 0.
+position[i] + 1 or position[i] - 1 with cost = 1.
+Return the minimum cost needed to move all the chips to the same position.
+
+            ====sol=========
+  class Solution {
+    public int minCostToMoveChips(int[] pos) {
+        int even=0,odd=0;
+        for(int i=0;i<pos.length;i++){
+            if(pos[i]%2==0){
+                even++;
+            }
+            else{
+                odd++;
+            }
+        }
+        return Math.min(even,odd);
+    }
+}

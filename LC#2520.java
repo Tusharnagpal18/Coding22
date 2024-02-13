04bbf5b11ad54b5923949566a36883c8c463ce0e
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2024-02-13 23:49:50 +0530

    Update LC#2520.java

--- LC#2520.java
+++ LC#2520.java
@@ -1,5 +1,5 @@
                                             Count the Digits That Divide a Number
-
+                                              
 Given an integer num, return the number of digits in num that divide num.
 
 An integer val divides nums if nums % val == 0.

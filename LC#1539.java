16922020cbabe90d8a3f65287dc42254c27c6918
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2024-03-10 22:05:52 +0530

    Update LC#1539.java

--- LC#1539.java
+++ LC#1539.java
@@ -1,5 +1,6 @@
 
-                                            1539. Kth Missing Positive Number                                           
+                                            1539. Kth Missing Positive Number     
+                                              
 Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
 Return the kth positive integer that is missing from this array.
  ============SOL==========

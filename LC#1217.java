44f957f83a308c0985163e4c930204cef580419a
Tushar Nagpal <68907030+Tusharnagpal18@users.noreply.github.com>
2024-04-04 23:48:24 +0530

    Update LC#1217.java

--- LC#1217.java
+++ LC#1217.java
@@ -1,6 +1,5 @@
 We have n chips, where the position of the ith chip is position[i].
 
-            
 We need to move all the chips to the same position. In one step, we can change the position of the ith chip from position[i] to:
 
 position[i] + 2 or position[i] - 2 with cost = 0.
